cfd2dd7d9abdf4425931364674a66f08d740b15f
Gordey Chernyy <kewava@gmail.com>
2018-01-06 14:38:03 -0800

    script 2

---
+++ Script2.js
@@ -0,0 +1,54 @@
+textureW = 1920;
+textureH = 1080;
+
+rearW = 1920;
+rearH = 540;
+
+frontW = 1920;
+frontH = 540;
+
+function Initialize() 
+{ 
+
+    layer = Document.FindLayer("Untitled Layer");
+
+	rearScaleY = rearH/textureH;
+	rearPosY = 1.0 - rearScaleY;
+
+	frontScaleX = frontW/textureW;
+	frontScaleY = frontH/textureH;
+	frontPosY = - (frontScaleY + ((rearScaleY*2)-1) );
+
+    Log("rearScaleY : " + rearScaleY);
+    Log("rearPosY : " + rearPosY);
+    Log("frontScaleX : " + frontScaleX);
+    Log("frontScaleY : " + frontScaleY);
+    Log("frontPosY : " + frontPosY);
+
+    for (var i = 0; i < layer.GetNumNodes(); i++) {
+    	node = layer.GetNode(i);
+    	if(node.GetName() == "Texture Rear"){
+    		node.SetFloat("Transform.Position Y", -0.5);
+    		node.SetFloat("Transform.Scale Y", rearScaleY);
+    	}else if(node.GetName() == "Texture Front"){
+			node.SetFloat("Transform.Position Y", 0.5);
+    		node.SetFloat("Transform.Scale Y", frontScaleY);
+    		node.SetFloat("Transform.Scale X", frontScaleX);
+    	}else if(node.GetName() == "Render Rear"){
+    		node.SetInt("Attributes.Width", rearW);
+    		node.SetInt("Attributes.Height", rearH);
+    	}else if(node.GetName() == "Render Front"){
+    		node.SetInt("Attributes.Width", frontW);
+    		node.SetInt("Attributes.Height", frontH);
+    	}else if(node.GetName() == "Front Aspect Ratio"){
+    		node.SetInt("Attributes.Width", frontW);
+    		node.SetFloat("Attributes.Value 1", frontW/frontH);
+    	}
+    }
+}
+
+function Update()  { 
+
+}
+
+Initialize();
